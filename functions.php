7fcda0cd9dae92642ff217e61a64b2784859d167
Isaac <aziksimpson@gmail.com>
2023-02-18 22:43:50 +0300

    подключает страницу регистрации

--- functions.php
+++ functions.php
@@ -3,26 +3,29 @@
  * Форматирует цену лота - разделяет пробелом разряды числа, добавляет знак рубля
  * @param integer $num Цена лота
  * @return string Как цена будет показываться в карточке
-*/
-function format_num ($num) {
+ */
+function format_num($num)
+{
     $num = ceil($num);
     $num = number_format($num, 0, '', ' ');
 
     return "$num ₽";
-    }
+}
 
-function console_log( $data ){
+function console_log($data)
+{
     echo '<script>';
-    echo 'console.log('. json_encode( $data ) .')';
+    echo 'console.log(' . json_encode($data) . ')';
     echo '</script>';
-    }
+}
 
 /**
  * Возвращеет количество целых часов и остатка минут от настоящего времени до даты
  * @param string $date Дата истечения времени
  * @return array
-*/
-function get_time_left ($date) {
+ */
+function get_time_left($date)
+{
     date_default_timezone_set('Europe/Moscow');
     $final_date = date_create($date);
     $cur_date = date_create("now");
@@ -39,10 +42,10 @@ function get_time_left ($date) {
     $res[] = $minutes;
 
     return $res;
-    }
+}
 
 
-    /**
+/**
  * Создает подготовленное выражение на основе готового SQL запроса и переданных данных
  *
  * @param $link mysqli Ресурс соединения
@@ -51,7 +54,8 @@ function get_time_left ($date) {
  *
  * @return stmt Подготовленное выражение
  */
-function db_get_prepare_stmt_version($link, $sql, $data = []) {
+function db_get_prepare_stmt_version($link, $sql, $data = [])
+{
     $stmt = mysqli_prepare($link, $sql);
 
     if ($stmt === false) {
@@ -68,8 +72,7 @@ function db_get_prepare_stmt_version($link, $sql, $data = []) {
 
             if (is_int($value)) {
                 $type = 'i';
-            }
-            else if (is_double($value)) {
+            } else if (is_double($value)) {
                 $type = 'd';
             }
 
@@ -96,7 +99,8 @@ function db_get_prepare_stmt_version($link, $sql, $data = []) {
  * @param object $result_query mysqli Результат запроса к базе данных
  * @return array
  */
-function get_arrow ($result_query) {
+function get_arrow($result_query)
+{
     $row = mysqli_num_rows($result_query);
     if ($row === 1) {
         $arrow = mysqli_fetch_assoc($result_query);
@@ -114,17 +118,20 @@ function get_arrow ($result_query) {
  * @param array $allowed_list Список существующих категорий
  * @return string Текст сообщения об ошибке
  */
-function validate_category ($id, $allowed_list) {
+function validate_category($id, $allowed_list)
+{
     if (!in_array($id, $allowed_list)) {
         return "Указана несуществующая категория";
     }
 }
+
 /**
  * Проверяет что содержимое поля является числом больше нуля
  * @param string $num число которое ввел пользователь в форму
  * @return string Текст сообщения об ошибке
  */
-function validate_number ($num) {
+function validate_number($num)
+{
     if (!empty($num)) {
         $num *= 1;
         if (is_int($num) && $num > 0) {
@@ -132,14 +139,17 @@ function validate_number ($num) {
         }
         return "Содержимое поля должно быть целым числом больше ноля";
     }
-};
+}
+
+;
 
 /**
  * Проверяет что дата окончания торгов не меньше одного дня
  * @param string $date дата которую ввел пользователь в форму
  * @return string Текст сообщения об ошибке
  */
-function validate_date ($date) {
+function validate_date($date)
+{
     if (is_date_valid($date)) {
         $now = date_create("now");
         $d = date_create($date);
@@ -152,4 +162,35 @@ function validate_date ($date) {
     } else {
         return "Содержимое поля «дата завершения» должно быть датой в формате «ГГГГ-ММ-ДД»";
     }
-};
+}
+
+;
+//New
+/**
+ * Проверяет что содержимое поля является корректным адресом электронной почты
+ * @param string $email адрес электронной почты
+ * @return string Текст сообщения об ошибке
+ */
+function validate_email($email)
+{
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return "E-mail должен быть корректным";
+    }
+}
+
+/**
+ * Проверяет что содержимое поля укладывается в допустимый диапазон
+ * @param string $value содержимое поля
+ * @param int $min минимальное количество символов
+ * @param int $max максимальное количество символов
+ * @return string Текст сообщения об ошибке
+ */
+function validate_length($value, $min, $max)
+{
+    if ($value) {
+        $len = strlen($value);
+        if ($len < $min or $len > $max) {
+            return "Значение должно быть от $min до $max символов";
+        }
+    }
+}
--- models.php
+++ models.php
@@ -4,7 +4,8 @@
  * @param string $date Дата в виде строки, в формате 'YYYY-MM-DD'
  * @return string SQL-запрос
  */
-function get_query_list_lots ($date) {
+function get_query_list_lots($date)
+{
     return "SELECT lots.id, lots.title, lots.start_price, lots.img, lots.date_finish, categories.name_category FROM lots
     JOIN categories ON lots.category_id=categories.id
     WHERE date_creation > $date ORDER BY date_creation DESC;";
@@ -15,40 +16,103 @@ function get_query_list_lots ($date) {
  * @param integer $id_lot id лота
  * @return string SQL-запрос
  */
-function get_query_lot ($id_lot) {
+function get_query_lot($id_lot)
+{
     return "SELECT lots.title, lots.start_price, lots.img, lots.date_finish, lots.lot_description, categories.name_category FROM lots
     JOIN categories ON lots.category_id=categories.id
     WHERE lots.id=$id_lot;";
 }
+
 /**
  * Формирует SQL-запрос для создания нового лота
  * @param integer $user_id id пользователя
  * @return string SQL-запрос
  */
-function get_query_create_lot ($user_id) {
+function get_query_create_lot($user_id)
+{
     return "INSERT INTO lots (title, category_id, lot_description, start_price, step, date_finish, img, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, $user_id);";
 }
+
 /**
  * Возвращает массив категорий
  * @param $con Подключение к MySQL
- * @return $error Описание последней ошибки подключения
- * @return array $categuries Ассоциативный массив с категориями лотов из базы данных
+ * @return [Array | String] $categuries Ассоциативный массив с категориями лотов из базы данных
+ * или описание последней ошибки подключения
  */
-function get_categories ($con) {
+function get_categories($con)
+{
     if (!$con) {
-    $error = mysqli_connect_error();
-    return $error;
+        $error = mysqli_connect_error();
+        return $error;
     } else {
         $sql = "SELECT id, character_code, name_category FROM categories;";
         $result = mysqli_query($con, $sql);
         if ($result) {
             $categories = get_arrow($result);
             return $categories;
-            } else {
-                $error = mysqli_error($con);
-                return $error;
-                }
         }
+        $error = mysqli_error($con);
+        return $error;
+    }
 }
 
+//New
+/**
+ * Возвращает массив данных пользователей: адресс электронной почты и имя
+ * @param $con Подключение к MySQL
+ * @return [Array | String] $users_data Двумерный массив с именами и емейлами пользователей
+ * или описание последней ошибки подключения
+ */
+function get_users_data($con)
+{
+    if (!$con) {
+        $error = mysqli_connect_error();
+        return $error;
+    } else {
+        $sql = "SELECT email, user_name FROM users;";
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+            $users_data = get_arrow($result);
+            return $users_data;
+        }
+        $error = mysqli_error($con);
+        return $error;
+    }
+}
+
+/**
+ * Формирует SQL-запрос для регистрации нового пользователя
+ * @param integer $user_id id пользователя
+ * @return string SQL-запрос
+ */
+function get_query_create_user()
+{
+    return "INSERT INTO users (date_registration, email, user_password, user_name, contacts) VALUES (NOW(), ?, ?, ?, ?);";
+}
+
+
+//login
+/**
+ * Возвращает массив данных пользователя: id адресс электронной почты имя и хеш пароля
+ * @param $con Подключение к MySQL
+ * @param $email введенный адрес электронной почты
+ * @return [Array | String] $users_data Массив с данными пользователя: id адресс электронной почты имя и хеш пароля
+ * или описание последней ошибки подключения
+ */
+function get_login($con, $email)
+{
+    if (!$con) {
+        $error = mysqli_connect_error();
+        return $error;
+    } else {
+        $sql = "SELECT id, email, user_name, user_password FROM users WHERE email = '$email'";
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+            $users_data = get_arrow($result);
+            return $users_data;
+        }
+        $error = mysqli_error($con);
+        return $error;
+    }
+}
 
